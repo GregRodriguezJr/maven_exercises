226f6276c33c27e89c722fed6d42205ea8d13cd0
Greg Rodriguez <greg.c.rodriguez.jr@gmail.com>
2022-10-19 16:55:55 -0500

    completed exercise

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,18 @@
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Scanner scanner = new Scanner(System.in);
+
+        // a. Tells whether or not what the user entered is a number
+        // b. Flips the case of the string
+        // c. Reverses the string
+        System.out.print("Enter a string value: ");
+        String userInput = scanner.next();
+        System.out.println("Entered number: " + StringUtils.isNumeric(userInput));
+        System.out.println("Case flip: " + StringUtils.swapCase(userInput));
+        System.out.println("Value flipped: " + StringUtils.reverse(userInput));
     }
 }
